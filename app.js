9e05ec687970ae13032963586da3088bbbc729e5
Manish Jha <jham8423@gmail.com>
2024-08-07 12:14:20 +0530

    all files are addede

---
+++ app.js
@@ -0,0 +1,87 @@
+// import express from "express";
+// import { dbConnection } from "./database/dbConnection.js";
+// import jobRouter from "./routes/jobRoutes.js";
+// import userRouter from "./routes/userRoutes.js";
+// import applicationRouter from "./routes/applicationRoutes.js";
+// import { config } from "dotenv";
+// import cors from "cors";
+// import { errorMiddleware } from "./middlewares/error.js";
+// import cookieParser from "cookie-parser";
+// import fileUpload from "express-fileupload";
+
+// const app = express();
+// config({ path: "./config/config.env" });
+
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL],
+//     method: ["GET", "POST", "DELETE", "PUT"],
+//     credentials: true,
+//   })
+// );
+
+// app.use(cookieParser());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.use(
+//   fileUpload({
+//     useTempFiles: true,
+//     tempFileDir: "/tmp/",
+//   })
+// );
+// app.use("/api/v1/user", userRouter);
+// app.use("/api/v1/job", jobRouter);
+// app.use("/api/v1/application", applicationRouter);
+// dbConnection();
+
+// app.use(errorMiddleware);
+// export default app;
+
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import jobRouter from "./routes/jobRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import applicationRouter from "./routes/applicationRoutes.js";
+import { config } from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
+const app = express();
+config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(helmet()); // Add security headers
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 })); // Limit repeated requests to public APIs
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
---
+++ database/dbConnection.js
@@ -0,0 +1,30 @@
+// import mongoose from "mongoose";
+
+// export const dbConnection = () => {
+//   mongoose
+//     .connect(process.env.MONGO_URI, {
+//       dbName: "MERN_JOB_SEEKING_WEBAPP",
+//     })
+//     .then(() => {
+//       console.log("Connected to database.");
+//     })
+//     .catch((err) => {
+//       console.log(`Some Error occured. ${err}`);
+//     });
+// };
+
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "MERN_JOB_SEEKING_WEBAPP",
+    })
+    .then(() => {
+      console.log("Connected to database.");
+    })
+    .catch((err) => {
+      console.error(`Database connection error: ${err.message}`);
+      process.exit(1); // Exit the process on failure
+    });
+};
